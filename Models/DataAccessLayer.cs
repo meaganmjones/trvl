a1bf7a2b72b512049a5dd54ae4d22b7b986b4af3
meaganmjones <mmjones@email.neit.edu>
2022-08-02 12:20:55 -0400

    Add project files.

---
+++ Models/DataAccessLayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+using Microsoft.Extensions.Configuration;
+using Trvl.Models;
+
+namespace Trvl.Models
+{
+    public class DataAccessLayer
+    {
+        string connectionString;
+
+        private readonly IConfiguration _configuration;
+
+        public DataAccessLayer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DeafaultConnection");
+        }
+
+        //public TrvlModel GetSearch(string? search)
+        //{
+        //    TrvlModel prod = new TrvlModel();
+
+        //    try
+        //    {
+        //        using (SqlConnection con = new SqlConnection(connectionString))
+        //        {
+        //            string strSql = "SELECT * FROM trvl_Product WHERE ProdName = '%" + search + "%'";
+        //            SqlCommand cmd = new SqlCommand(strSql, con);
+        //            cmd.CommandType = CommandType.Text;
+
+        //            cmd.Parameters.AddWithValue("@ProdName");
+        //        }
+        //    }catch(Exception err)
+        //    {
+        //        search.Feedback = "ERROR: " + err.Message;
+        //    }
+        //    return search;
+        //}
+    }
+}
---
+++ Models/TrvlModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trvl.Models
+{
+    public class TrvlModel
+    {
+        public String Search_String { get; set; } //from search bar
+
+        public String Feedback { get; set; } //For feedback if it ever works... 
+    }
+}
